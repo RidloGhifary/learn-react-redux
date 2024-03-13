d329f03078445a99b6c012c6990ff39841f7e75f
RidloGhifary <ridloghfry@gmail.com>
2024-03-13 22:44:04 +0700

    connect db

--- be/src/database.js
+++ be/src/database.js
@@ -1,10 +1,10 @@
-import mysql from "mysql";
+import mysql from "mysql2";
 
 const db = mysql.createConnection({
   host: "localhost",
   user: "root",
+  password: "1234Ridlo",
   database: "testRedux",
-  password: "",
 });
 
 export default db;
--- be/src/index.js
+++ be/src/index.js
@@ -1,9 +1,24 @@
 import express from "express";
+import db from "./database.js";
 import cors from "cors";
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+
+app.get("/users", (req, res) => {
+  try {
+    const q = "SELECT * FROM users";
+
+    db.query(q, (err, result) => {
+      if (err) return res.status(500);
+
+      return res.status(200).json(result);
+    });
+  } catch (err) {
+    console.log(err);
+  }
+});
 
 app.listen(2024, () => console.log("Server running..."));
